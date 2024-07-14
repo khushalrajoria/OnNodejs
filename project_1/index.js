4a9faf302d853a7b4be7f4ca4bc0cfb747316a8b
Khushal rajoria <rajoriakhushal@gmail.com>
2024-07-15 00:54:12 +0530

    middlewares

--- project_1/index.js
+++ project_1/index.js
@@ -7,6 +7,28 @@ const PORT = 8000;
 
 //middleware
 app.use(express.urlencoded({extended: false})); // ---> imp this will help in parsing the json received from postman
+
+// app.use((req,res,next)=>{
+//     req.username="khushalraj"; // now this will be avaiable to next middleware and even the end server
+//     console.log("hello from middleware 1"); // if we leave it at that then the server will keep on running as we are not returning and will be stuck
+//     // return res.json({msgs:"ram ram laddarr "}); // this is be retuned to the client and it will not reach the server
+//     next(); // now this time we will actually reach the response normally
+// })
+// app.use((req,res,next)=>{ // 1st will send data to 2 , 2 will end it    
+//     console.log("hello from middleware 2",users.username); 
+//     res.end("helo");
+// })
+
+// they can also be used for logging
+app.use((req,res,next)=>{
+    fs.appendFile("log.txt",`hello ${Date.now} : ${req.method} : ${req.path}\n`,
+        (err,data)=>{
+            next();
+        }
+    );
+    
+})
+
 // Routes
 // this is an example incase we want to create a hybrid server with both SSR and USR
 app.get('/users', (req, res) => { // agar join nhi karenge toh commas se sepereate ho jayengi values
